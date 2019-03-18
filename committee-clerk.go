d48cc5296b26439d32a4f9528bcdcacc7be4e5e7
Lawful Lazy <lawful.lazy@gmail.com>
2019-03-17 20:16:29 -0700

    Add command handler

--- committee-clerk.go
+++ committee-clerk.go
@@ -5,16 +5,30 @@ import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"log"
+	"strconv"
+	"strings"
+
 	"os"
 	"os/signal"
 	"syscall"
 )
 
+// Provided by auth.json
 type AuthSettings struct {
-	Token string
+	Token    string
+	ClientID int
 }
 
+type Command func(*discordgo.Session, *discordgo.MessageCreate) error
+
+// Configuration
 const Prefix = ";"
+
+var Commands map[string]Command = make(map[string]Command)
+
+func addCommand(name string, cmd Command) {
+	Commands[Prefix+name] = cmd
+}
 
 func main() {
 	// Decode auth.json and store it in the appropriate struct.
@@ -38,6 +52,9 @@ func main() {
 
 	dg.AddHandler(messageCreate)
 
+	// Add commands
+	addCommand("ping", ping)
+
 	// Start the bot
 	if err = dg.Open(); err != nil {
 		log.Fatal("error opening connection,", err)
@@ -45,6 +62,9 @@ func main() {
 
 	// Wait here until an interruption signal is received
 	fmt.Println("Committee clerk is now running. Press CTRL-C to exit.")
+	fmt.Println("Invite the Committee Clerk with this url:")
+	fmt.Println("https://discordapp.com/oauth2/authorize?client_id=" +
+		strconv.Itoa(auth.ClientID) + "&permissions=268445776&scope=bot")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
@@ -63,7 +83,25 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if m.Content == Prefix+"ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	cmdstr := strings.Split(m.Content, " ")[0]
+
+	if cmd, ok := Commands[cmdstr]; ok {
+		ch, err := s.Channel(m.ChannelID)
+
+		if err != nil {
+			log.Println(m.Author, "sent command", m.Content)
+		} else {
+			// This logically shouldn't happen, but just in case!
+			log.Println(m.Author, "from", "#"+ch.Name, "sent command", m.Content)
+		}
+
+		if err := cmd(s, m); err != nil {
+			log.Println("Error:", err)
+		}
 	}
+}
+
+func ping(s *discordgo.Session, m *discordgo.MessageCreate) error {
+	_, err := s.ChannelMessageSend(m.ChannelID, "Pong!")
+	return err
 }
